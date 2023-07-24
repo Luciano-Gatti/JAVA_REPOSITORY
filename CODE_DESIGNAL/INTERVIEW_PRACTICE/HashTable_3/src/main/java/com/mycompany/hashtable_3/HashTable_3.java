2ead1ce8cf8c18e39e705ae5a71ec100f1353b4a
lucianogatti <luciano.gatti.utn@gmail.com>
2023-07-24 16:10:40 -0300

    code signal excersice
    ejercicios de code signal

---
+++ CODE_DESIGNAL/INTERVIEW_PRACTICE/HashTable_3/src/main/java/com/mycompany/hashtable_3/HashTable_3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.hashtable_3;
+
+/**
+ *
+ * @author lucia
+ */
+public class HashTable_3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ CODE_DESIGNAL/INTERVIEW_PRACTICE/HashTable_4/src/main/java/com/mycompany/hashtable_4/HashTable_4.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.hashtable_4;
+
+/**
+ *
+ * @author lucia
+ */
+public class HashTable_4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
